a007380dba1042e319673beb01e62c26b75ed452
thenerdygeek <subhankarhalder@yahoo.com>
2018-04-03 22:04:57 +0530

    GPS Tracking Test

--- app/src/main/java/com/example/android/trackmeup/MainActivity.java
+++ app/src/main/java/com/example/android/trackmeup/MainActivity.java
@@ -1,13 +1,92 @@
 package com.example.android.trackmeup;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textview;
+    private GPSTracker gpsTracker;
+    private Button stop;
+    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stop = findViewById(R.id.stopTrack);
+        gpsTracker = new GPSTracker(this);
+        permissions();
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gpsTracker.stopUsingGPS();
+                Toast.makeText(MainActivity.this, String.valueOf(gpsTracker.getIsGPSTrackingEnabled()) , Toast.LENGTH_SHORT).show();
+                handler.removeCallbacksAndMessages(null);
+            }
+        });
+
+
+        handler = new Handler();
+        final int delay = 2000; //milliseconds
+
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                //do something
+                loc();
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+
+    private void loc() {
+
+
+        if (gpsTracker.getIsGPSTrackingEnabled())
+        {
+            String stringLatitude = String.valueOf(gpsTracker.latitude);
+            textview = (TextView)findViewById(R.id.latitude);
+            textview.setText(stringLatitude);
+
+            String stringLongitude = String.valueOf(gpsTracker.longitude);
+            textview = (TextView)findViewById(R.id.longitude);
+            textview.setText(stringLongitude);
+        }
+//        Toast.makeText(MainActivity.this, String.valueOf(gpsTracker.getIsGPSTrackingEnabled()) , Toast.LENGTH_SHORT).show();
+    }
+
+    public void permissions()
+    {
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
